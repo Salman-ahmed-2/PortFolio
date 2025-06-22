8a0d2b89698729667e818b08aa92d9d2d99385be
Salman Ahmed <salmanahmed32121@gmail.com>
2025-06-23 00:52:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+//  alert("this site is under progress");
+
+
+
+var typed = new Typed('#element', {
+      
+      strings: ['Software Engineer','Web Developer','Coder','PubG King Player'],
+      typeSpeed: 50,
+      backSpeed: 20,
+      loop:true
+    });
+
+
+
+
+  function scrollpro() {
+    window.scrollTo({
+      top: 690,       
+      behavior: 'smooth'
+    });
+
+
+   
+  }
